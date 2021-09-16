9a29eca1064323ff689c822d30a2f1d90f3294a1
paulinho255 <paulinho255@gmail.com>
2021-09-16 19:21:07 -0300

    Create a post method endpoint

--- src/main/java/com/springboot/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/springboot/bookstore/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.springboot.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.bookstore.DTO.CategoriaDTO;
 import com.springboot.bookstore.domain.Categoria;
@@ -37,5 +41,15 @@ public class CategoriaResource {
 		List<Categoria> list = categoriaService.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); 
 		return ResponseEntity.ok().body(listDto);
+	}
+	/* Envio de nova requisição
+	 * curl -d "@Fornecedor.json" -H "Content-Type: application/json" -X POST http://localhost:8080/api/fornecedor
+	 * 
+	 * */
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = categoriaService.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
