4fce956b03667d8eca51968156d12ad8a3290cf7
paulinho255 <paulinho255@gmail.com>
2021-09-17 20:38:11 -0300

    Adding a new exception handled by class DataIntegrityViolationException.

--- src/main/java/com/springboot/bookstore/exceptions/ResourceExceptionHandle.java
+++ src/main/java/com/springboot/bookstore/exceptions/ResourceExceptionHandle.java
@@ -16,5 +16,12 @@ public class ResourceExceptionHandle {
 				e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 
 }
--- src/main/java/com/springboot/bookstore/services/CategoriaService.java
+++ src/main/java/com/springboot/bookstore/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.springboot.bookstore.DTO.CategoriaDTO;
@@ -13,6 +14,7 @@ import com.springboot.bookstore.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
+	
 	@Autowired
 	private CategoriaRepository repository;
 
@@ -30,14 +32,20 @@ public class CategoriaService {
 		obj.setId(null);
 		return repository.save(obj);
 	}
+	
 	public Categoria update(Integer id, CategoriaDTO obj) {
 		Categoria categoria = findById(id);
 		categoria.setNome(obj.getNome());
 		categoria.setDescricao(obj.getDescricao());
 		return repository.save(categoria);
 	}
+	
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.springboot.bookstore.exceptions.DataIntegrityViolationException("Object Categoria cannot be deleted. There are books associate to the object. ");
+		}
 	}
 }
