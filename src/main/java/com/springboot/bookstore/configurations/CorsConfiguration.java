523f083739330094bed7f09fdb3811fbd8ce9a54
paulinho255 <paulinho255@gmail.com>
2021-11-05 11:21:00 -0300

    Added new class core configuration to accept cross origins.

---
+++ src/main/java/com/springboot/bookstore/configurations/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.springboot.bookstore.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+	@Bean
+    public WebMvcConfigurer corsConfigurer() 
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://192.168.200.100:4200");
+            }
+        };
+    }
+}
