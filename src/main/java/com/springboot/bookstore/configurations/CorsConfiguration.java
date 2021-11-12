33b95d2dcc3b5511aed95f69d6a00de3321e51eb
paulinho255 <paulinho255@gmail.com>
2021-11-12 15:59:54 -0300

    Fixed errors from crossorigin when the api try to send a delete method via http.

--- src/main/java/com/springboot/bookstore/configurations/CorsConfiguration.java
+++ src/main/java/com/springboot/bookstore/configurations/CorsConfiguration.java
@@ -1,20 +1,17 @@
 package com.springboot.bookstore.configurations;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
-	@Bean
-    public WebMvcConfigurer corsConfigurer() 
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://192.168.200.100:4200");
-            }
-        };
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+         .allowedOrigins("http://192.168.200.100:4200") 
+        	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
