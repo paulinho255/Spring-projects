7e53d91301174105b8c9520d1f9f97fee85f873d
paulinho255 <paulinho255@gmail.com>
2021-11-22 18:16:45 -0300

    Changed cors configuration to accept incoming domain from heroku.

--- src/main/java/com/springboot/bookstore/configurations/CorsConfiguration.java
+++ src/main/java/com/springboot/bookstore/configurations/CorsConfiguration.java
@@ -1,5 +1,7 @@
 package com.springboot.bookstore.configurations;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,10 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer{
+	@Value("${cors.configuration.urls}")
+	private String[] origins;
+	@Value("${cors.configuration.methods}")
+	private String[] methods;
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-         .allowedOrigins("http://192.168.200.100:4200") 
-        	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+         .allowedOrigins(origins) 
+        	.allowedMethods(methods);
     }
+    
 }
